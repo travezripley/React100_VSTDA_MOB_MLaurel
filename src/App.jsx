a2af53df044f7fc0381ecacd4c7163c5ab1aedb2
Mike Laurel <mlaurel@gmail.com>
2019-04-08 14:37:38 -0700

    adds blankState and EditTodo UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className='container mt-3'>
+      <div className='container mt-3 mb-3'>
         <div className='row'>
           <h1 className='col-12 text-white'>Very Simple Todo App</h1>
           <p className='col-12 text-white'>Track all of the things</p>
@@ -38,7 +38,40 @@ class App extends Component {
             <div className='card'>
               <div className='card-header'>View Todos</div>
               <div className='card-body p-0'>
+                {/* Blank State */}
+                <div className='row alert alert-primary no-gutters m-0 rounded-0'>
+                  <div className='col-12'>
+                    <p>
+                        <span className='font-weight-bold'>
+                                                Welcome to Very Simple Todo App!
+                                            </span>
+                        <br />
+                                            Get started now by adding a new todo on the left
+                                        </p>
+                  </div>
+                </div>
+                {/* Edit Form */}
                 <div className='row alert alert-success no-gutters m-0 rounded-0'>
+                  <div className='form-group col-12'>
+                    <label htmlFor='todo' className='font-weight-bold'>
+                                            Description
+                                        </label>
+                    <textarea className='form-control' id='todo' rows='3' />
+                  </div>
+                  <div className='form-group col-6'>
+                    <label htmlFor='priority' className='font-weight-bold'>
+                                            Priority
+                                        </label>
+                    <select className='form-control' id='priority'>
+                        <option>Select a Priority</option>
+                      </select>
+                  </div>
+                  <div className='form-group col-1 offset-11 align-items-end'>
+                    <button className='btn btn-success'>Save</button>
+                  </div>
+                </div>
+                {/* Todo items */}
+                <div className='row alert alert-warning no-gutters m-0 rounded-0'>
                   <div className='col-11'>
                     <div className='form-group form-check m-0'>
                         <input
@@ -59,7 +92,7 @@ class App extends Component {
                     <i className='fas fa-edit blue float-right p-1' />
                   </div>
                 </div>
-                <div className='row alert alert-warning no-gutters m-0 rounded-0'>
+                <div className='row alert alert-success no-gutters m-0 rounded-0'>
                   <div className='col-11'>
                     <div className='form-group form-check m-0'>
                         <input
